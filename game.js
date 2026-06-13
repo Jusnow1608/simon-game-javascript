2b11ed2855ea165d9b9f0c898ca58ec46f79f3f7
Justyna <justyna.nowak.programista@gmail.com>
2026-06-13 23:13:48 +0200

    Added start of the game

--- game.js
+++ game.js
@@ -4,30 +4,48 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var  isGameStarted = false;
+
+var level = 0;
+
+  $(document).on("keydown", function(){
+    if (!isGameStarted){
+    
+    $("#level-title").text("Level "+ level);
+    nextSequence();
+    isGameStarted = true;
+    }
+  });
+
+
 $(".btn").on("click", function() {
               
-      var userChosenColour = $(this).attr("id");
-      userClickedPattern.push(userChosenColour);
-      console.log(userClickedPattern);
-      playSound(userChosenColour);
-      animatePress(userChosenColour);
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    //console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
  });
 
+
 function playSound(name){
- var soundSource = "sounds/"+name + ".mp3";
+  var soundSource = "sounds/"+name + ".mp3";
   var sound = new Audio(soundSource);
   sound.play();
 }
 
 function nextSequence(){
+  
+  level++;
+  $("#level-title").text("Level "+level);
+
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   var activeButton = $("#"+randomChosenColour);
 
-  activeButton.animate({opacity:0}, 0.3);
-  activeButton.animate({opacity:1});
+  activeButton.animate({opacity:0}, 300).animate({opacity:1});
 
  //$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
@@ -36,7 +54,7 @@ function nextSequence(){
  function animatePress(currentColour){
 
   $("#"+currentColour).addClass("pressed");
-  
+
  setTimeout(function() {
         $("#"+currentColour).removeClass("pressed");
     }, 100);
